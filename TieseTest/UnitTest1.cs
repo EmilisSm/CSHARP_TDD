83797a04cc0b1a99d5ab42242548a0f549595180
EmilisSm <emax123456@gmail.com>
2018-04-25 15:30:45 +0300

    Add project files.

---
+++ TieseTest/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest;
+
+namespace TieseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TiesiesEqual()
+        {
+            Tiese tiese1 = new Tiese(1, 2, 4, 2);
+            Tiese tiese2 = new Tiese(1, 4, 4, 4);
+
+            int expected = tiese2.TiesesIlgis();
+            int actual = tiese1.TiesesIlgis();
+
+            Assert.AreEqual(expected, actual, 1, "Tieses nera lygios"); 
+        }
+
+        [TestMethod]
+        public void TiesesNotEqual()
+        {
+            Tiese tiese1 = new Tiese(1, 6, 6, 2);
+            Tiese tiese2 = new Tiese(1, 2, 4, 2);
+
+            int expected = tiese2.TiesesIlgis();
+            int actual = tiese1.TiesesIlgis();
+
+            Assert.AreNotEqual(expected, actual, 1, "Tieses lygios");
+        }
+
+        [TestMethod]
+        public void TieseNotNull()
+        {
+            Tiese tiese1 = new Tiese(1, 6, 6, 2);
+
+            int expected = tiese1.TiesesIlgis();
+
+            Assert.IsNotNull(expected, "Tiese is null");
+        }
+
+        [TestMethod]
+        public void KryptiesKofNotNUll()
+        {
+            Tiese tiese1 = new Tiese(1, 6, 6, 2);
+
+            int expected = tiese1.KryptiesKof();
+
+            Assert.IsNotNull(expected, "Krypties Kof is null");
+        }
+
+        [TestMethod]
+        public void KryptiesKofEqual()
+        {
+            Tiese tiese1 = new Tiese(1, 2, 4, 2);
+            Tiese tiese2 = new Tiese(1, 4, 4, 4);
+
+            int expected = tiese2.KryptiesKof();
+            int actual = tiese1.KryptiesKof();
+
+            Assert.AreEqual(expected, actual, "Krypties Kof nera lygus");
+        }
+
+        [TestMethod]
+        public void KryptiesKofNotEqual()
+        {
+            Tiese tiese1 = new Tiese(1, 6, 4, 2);
+            Tiese tiese2 = new Tiese(1, 2, 4, 2);
+
+            int expected = tiese2.KryptiesKof();
+            int actual = tiese1.KryptiesKof();
+
+            Assert.AreNotEqual(expected, actual, -1, "Krypties Kof lygus");
+        }
+    }
+}
---
+++ UnitTest/Tiese.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class Tiese
+    {
+        int _x1;
+        int _y1;
+        int _x2;
+        int _y2;
+
+        public Tiese(int x1, int y1, int x2, int y2)
+        {
+            _x1 = x1;
+            _y1 = y1;
+            _x2 = x2;
+            _y2 = y2;
+        }
+
+        public int KryptiesKof()
+        {
+            int y = _y2 - _y1;
+            int x = _x2 - _x1;
+
+            int k = y / x;
+
+            Console.WriteLine("Krypties koeficientas: {0} ", k);
+
+            return k;
+
+
+        }
+
+        public int TiesesIlgis()
+        {
+            int y = _y2 - _y1;
+            int x = _x2 - _x1;
+
+            int i = y + x;
+
+
+            Console.WriteLine("Tieses ilgis: {0} ", i);
+
+            return i;
+
+        }
+
+
+
+    }
+}
